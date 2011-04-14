20b451de8864d0c6c96bfbbab5b3408e0b64029c
aloneroad <aloneroad@gmail.com>
2011-04-14 11:54:56 +0700

    more results

--- src/mem_leak.py
+++ src/mem_leak.py
@@ -5,15 +5,14 @@ import celery
 from celery.decorators import task
 
 def objects_count():
-  gc.set_debug(gc.DEBUG_SAVEALL)
   gc.collect()
   return len(gc.get_objects())
 
 def write_down(celery_version, count, objects_in_memory):
   filename = celery_version + '.txt'
   open(filename, 'a').write("%s\t%s\n" % (count, objects_in_memory))
+  print '%s\t\t%s' % (count, objects_in_memory)
   return True
-  
 
 @task
 def foobar():
@@ -21,17 +20,13 @@ def foobar():
 
 if __name__ == "__main__":
   print "Celery version: ", celery.__version__
+  print "messages_sent\tobjects_in_memory"
   
-  count = 0
-  objects_in_memory = objects_count()
-  
-  print "At the beginning: %s objects in memory" % objects_in_memory
-  write_down(celery.__version__, count, objects_in_memory)
+  count = 0  
+  write_down(celery.__version__, count, objects_count())
   
   while True:
     foobar.delay()
-    count += 1
+    count += 1 
     if count % 100 == 0:
-      objects_in_memory = objects_count()
-      print "After %s messages sent: %s objects in memory" % (count, objects_in_memory)
-      write_down(celery.__version__, count, objects_in_memory)
+      write_down(celery.__version__, count, objects_count())
